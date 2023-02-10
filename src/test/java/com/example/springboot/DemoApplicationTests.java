9aab0dcd924cf00c94a143a550c968fa488f1b91
rogodard <sieskas@gmail.com>
2023-02-10 12:53:26 -0500

    test sonar

--- src/test/java/com/example/springboot/DemoApplicationTests.java
+++ src/test/java/com/example/springboot/DemoApplicationTests.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest(
-//		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-//		classes = DemoApplication.class)
-//@ActiveProfiles("integration")
-//class DemoApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//		assertEquals("com.example.springboot.DemoApplicationTests", this.getClass().getName());
-//	}
-//}
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		classes = DemoApplication.class)
+@ActiveProfiles("integration")
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals("com.example.springboot.DemoApplicationTests", this.getClass().getName());
+	}
+}
