a081bd36c55f1f4b0240fa6adf45e777c2b4f13a
rogodard <sieskas@gmail.com>
2023-02-10 13:05:16 -0500

    test sonar

--- src/test/java/com/example/springboot/DemoApplicationTests.java
+++ src/test/java/com/example/springboot/DemoApplicationTests.java
@@ -3,13 +3,14 @@ package com.example.springboot;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 		classes = DemoApplication.class)
-@ActiveProfiles("integration")
 class DemoApplicationTests {
 
 	@Test
