5780512184002b0e050e1476f104d5151ecc5e18
rogodard <sieskas@gmail.com>
2023-02-11 13:10:40 -0500

    add jacoco and prop

--- src/main/java/com/example/springboot/DemoApplication.java
+++ src/main/java/com/example/springboot/DemoApplication.java
@@ -2,9 +2,12 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer
+		implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/test/java/com/example/springboot/functionals/RunCucumberTests.java
+++ src/test/java/com/example/springboot/functionals/RunCucumberTests.java
@@ -1,6 +1,15 @@
 package com.example.springboot.functionals;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com.example.springboot.functionals")
+@ConfigurationParameter(
+		key = Constants.GLUE_PROPERTY_NAME,
+		value = "com.example.springboot.functionals")
 public class RunCucumberTests {}
